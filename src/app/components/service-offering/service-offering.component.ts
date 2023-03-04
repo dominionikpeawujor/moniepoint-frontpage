50b973df6f726de485679465e9dddfcc61392988
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-03-04 12:48:01 +0100

    import the service into the service offering component and get the data from the json. Also, use the ngFor directive to send the gotten data to the service offering child component for use

--- src/app/components/service-offering/service-offering.component.ts
+++ src/app/components/service-offering/service-offering.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceCard } from 'src/app/models/service-offering-card';
+import { ServiceOfferingService } from 'src/app/services/service-offering.service';
 
 @Component({
   selector: 'app-service-offering',
   templateUrl: './service-offering.component.html',
   styleUrls: ['./service-offering.component.css']
 })
-export class ServiceOfferingComponent {
+export class ServiceOfferingComponent implements OnInit{
 
+  services: ServiceCard[] = [];
+  constructor(private serviceOfferingService:ServiceOfferingService) {}
+
+  ngOnInit(): void {
+    this.serviceOfferingService.getData().subscribe(data => {
+      this.services = data;
+    })
+  }
 }
