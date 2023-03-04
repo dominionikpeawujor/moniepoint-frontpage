3aadee114792153348c52ab641c4283a8135d143
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-03-04 13:43:47 +0100

    initialize input variable of service offering card to receive data from parent component i.e. service offering.

--- src/app/components/service-offering-card/service-offering-card.component.ts
+++ src/app/components/service-offering-card/service-offering-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ServiceCard } from 'src/app/models/service-offering-card';
 
 @Component({
   selector: 'app-service-offering-card',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./service-offering-card.component.css']
 })
 export class ServiceOfferingCardComponent {
+
+  @Input() service: ServiceCard;
+
+  constructor() {
+    this.service = {
+      id: '1',
+      service: '',
+      url: '',
+      description:''
+
+    }
+  }
 
 }
