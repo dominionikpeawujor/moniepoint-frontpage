1ab8f37ad28aaf0ee9ff65ac621d2e2af0e6155a
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-03-04 01:50:45 +0100

    declared json value to aid service

--- src/app/services/service-offering.service.ts
+++ src/app/services/service-offering.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ServiceCard } from '../models/service-offering-card';
 
 @Injectable({
   providedIn: 'root'
