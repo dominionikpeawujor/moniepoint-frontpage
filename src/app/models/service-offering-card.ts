98476992fa2d986b70def6ece3a2af681cc4a239
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-03-04 01:51:24 +0100

    create a service offering model

---
+++ src/app/models/service-offering-card.ts
@@ -0,0 +1,17 @@
+export class ServiceCard {
+  id: string;
+  name: string;
+  price: number;
+  url: string;
+  description: string;
+  quantity: number;
+
+  constructor() {
+    this.id = '1';
+    this.name = '';
+    this.price = 0;
+    this.url = '';
+    this.description = '';
+    this.quantity = 0;
+  }
+}
