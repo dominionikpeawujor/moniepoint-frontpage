e9ac55f22a558da1afd8ee8855852cac2b54a768
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-03-04 01:46:10 +0100

    add http client module to the app module and the http client to the service-offering service

--- src/app/services/service-offering.service.ts
+++ src/app/services/service-offering.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceOfferingService {
+  private _jsonURL = 'assets/data.json';
 
   constructor() { }
 }
